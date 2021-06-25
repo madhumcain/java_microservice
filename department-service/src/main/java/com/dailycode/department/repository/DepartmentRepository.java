16afc4d2dc9ba0f165903c88d14a4913d11e03ef
madhu sudhanan <madhumcain@gmail.com>
2021-06-25 10:07:19 +0200

    first commit

---
+++ department-service/src/main/java/com/dailycode/department/repository/DepartmentRepository.java
@@ -0,0 +1,16 @@
+package com.dailycode.department.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dailycode.department.entity.Department;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Long>{
+
+
+	Department findByDepartmentId(Long departmentId);
+
+	
+	}
+
